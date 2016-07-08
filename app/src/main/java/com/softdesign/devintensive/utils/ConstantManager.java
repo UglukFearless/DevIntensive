612eb83da0abf85460ce385476f81af04fcadd7f
Aleksey Danilov <UglukFearless@mail.ru>
2016-07-09 01:38:35 +0700

    add login activity, add work with intents

--- app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -1,5 +1,7 @@
 package com.softdesign.devintensive.utils;
 
+import android.content.IntentSender;
+
 /**
  * Created by Ugluk on 24.06.2016.
  */
@@ -12,4 +14,14 @@ public interface ConstantManager {
     String USER_VK_KEY = "USER_KEY_VK";
     String USER_GIT_KEY = "USER_KEY_GIT";
     String USER_BIO_KEY = "USER_KEY_BIO";
+    String USER_PHOTO_KEY = "USER_PHOTO_KEY";
+
+
+    int LOAD_PROFILE_PHOTO = 1;
+    int REQUEST_CAMERA_PICTURE = 99;
+    int REQUEST_GALLERY_PICTURE = 88;
+
+    int PERMISSION_REQUEST_SETTINGS_CODE = 101;
+    int CAMERA_REQUEST_PERMISSION_CODE = 102;
+    int CALL_PHONE_REQUEST_PERMISSIONS_CODE = 103;
 }
